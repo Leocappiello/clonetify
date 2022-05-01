f397476d981dfba594ac187e455fecf73c305409
Leocappiello <leonelcappiello@gmail.com>
2022-04-30 21:12:10 -0300

    Pagination active

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,29 +1,40 @@
 import { useEffect } from "react"
 
-const Pagination = ({totalAlbums, albumPerPages, paginate, currentPage}) => {
+const Pagination = ({ totalAlbums, albumPerPages, paginate, currentPage }) => {
     const pageNumbers = []
 
-    for(let i = 0; i<Math.ceil(totalAlbums / albumPerPages); i++){
+    for (let i = 1; i < Math.ceil(totalAlbums / albumPerPages); i++) {
         pageNumbers.push(i)
     }
-    
+
     useEffect(() => {
         console.log(currentPage)
     })
-    
-    return(
+
+
+    return (
         <>
-        <nav>
-            <ul className="pagination justify-content-center mt-3">
-                {pageNumbers.map(number => (
-                    <li key={number} className="page-item">
-                        <a href="!#" onClick={() => {paginate(number)}} className="page-link bg-dark" >
-                            {number}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+            <nav>
+                <ul className="pagination justify-content-center mt-3">
+                    {pageNumbers.map((number) => {
+                        if (number === currentPage) {
+                            return (
+                                <li key={number} className="page-item active">
+                                    <a href="!#" onClick={() => { paginate(number) }} className="page-link bg-dark" >
+                                        {number}
+                                    </a>
+                                </li>
+                            )
+                        } else {
+                            return (<li key={number} className="page-item">
+                                <a href="!#" onClick={() => { paginate(number) }} className="page-link bg-dark" >
+                                    {number}
+                                </a>
+                            </li>)
+                        }
+                    })}
+                </ul>
+            </nav>
         </>
     )
 }
